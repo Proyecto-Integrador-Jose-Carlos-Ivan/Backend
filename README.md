490e7c63637300c8e95a63cb0b8bdd3d77dca0fe
jose51igual <avila51jose@gmail.com>
2025-02-12 15:44:04 +0100

    added user admin for testing

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Proyecto integrador 2º DAW B - Jose Igual Avila - Carlos Garcia Romero - Ivan J
 
 
 Comando para parar todos los contenedores de docker : `docker stop $(docker ps -q)`
+
+Usuario para probar la api , que genera el token : `localhost/api/login`
+
+Usuario: admin@example.com
+Contraseña: password
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'administrador',
+        ]);
         User::factory(10)->create();
     }
 }
