1520fdbaa4bc8387723efa73bdaf2e5a65a2fb35
jose51igual <avila51jose@gmail.com>
2025-02-19 02:37:35 +0100

    Update environment configuration and deployment settings; correct database name casing and modify server details

--- deploy.php
+++ deploy.php
@@ -5,17 +5,17 @@ require 'recipe/laravel.php';
 
 // Config
 
-set('repository', 'https://github.com/Proyecto-Integrador-Jose-Carlos-Ivan/Backend.git');
+set('repository', 'https://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@github.com/Proyecto-Integrador-Jose-Carlos-Ivan/Backend.git');
 
 set('branch', 'develop');
 
 add('shared_files', ['.env']);
 
 // Hosts toca cambiar la ip por la de nuestro servidor, el usuario por el nuestro y la clave por la nuestra y el deploy path
-host('23.21.148.130') 
-    ->set('remote_user', 'prod-ud4-deployer')
-    ->set('identity_file', '~/Descargas/todo/servidor/lemp/lemp.pem')
-    ->set('deploy_path', '/var/www/prod-ud4-a4/html');
+host('3.220.165.74') 
+    ->set('remote_user', 'deployer')
+    ->set('identity_file', '/home/jose/Descargas/Clave-Proyecto.pem')
+    ->set('deploy_path', '/var/www/Backend');
 
 // Tasks
 task('upload:env', function () {
@@ -34,8 +34,15 @@ task('reload:php-fpm', function () {
     run('sudo /etc/init.d/php8.3-fpm restart');
     });
 
+    task('deploy:install_acl', function () {
+        run('sudo apt-get update');
+        run('sudo apt-get install -y acl');
+    })->desc('Install ACL package');
+
 // Hooks
 before('deploy:shared', 'upload:env');
 before('deploy:symlink', 'artisan:migrate:fresh:seed');
+before('deploy:writable', 'deploy:install_acl');
+
 after('deploy', 'reload:php-fpm');
 after('deploy:failed', 'deploy:unlock');
